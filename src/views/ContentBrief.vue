f7bf6884c40481843e3976babc04fd6a4b65415c
Jakkaphan Payuhakait <tee@dept.email>
2021-04-16 15:27:23 +0700

    Add content brief animation

--- src/views/ContentBrief.vue
+++ src/views/ContentBrief.vue
@@ -1,37 +1,77 @@
 <template>
   <div class="content-brief-page">
-    <div class="content-brief-text">
-      ความเป็นชายมิหายเพราะงานบ้าน<br/>
-      โปรดอย่าคร้านว่ามิใช่หน้าที่<br/>
-      แล้วผลักไสงานนั้นให้สตรี<br/>
-      คู่ที่ดีต้องมีความเท่าเทียม
+    <div
+      class="content-brief-text"
+      :class="isPageInViewport ? 'show-brief-text' : ''"
+      align="center"
+    >
+      <div>
+        <p>ความเป็นชายมิหายเพราะงานบ้าน</p>
+      </div>
+      <div>
+        <p>โปรดอย่าคร้านว่ามิใช่หน้าที่</p>
+      </div>
+      <div>
+        <p>แล้วผลักไสงานนั้นให้สตรี</p>
+      </div>
+      <div>
+        <p>คู่ที่ดีต้องมีความเท่าเทียม</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import scrollEventCheck from '@/helpers/scrollEventCheck'
+
 export default {
-  name: 'ContentBrief'
+  name: 'ContentBrief',
+  data() {
+    return {
+      isPageInViewport: false
+    }
+  },
+  mounted() {
+    document.addEventListener('scroll', this.scrollListener)
+  },
+  methods: {
+    scrollListener() {
+      const contentBriefContainer = document.querySelector('.content-brief-page')
+      const isInViewport = scrollEventCheck.isInViewport(contentBriefContainer)
+      if (isInViewport) {
+        this.isPageInViewport = true
+      }
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 $theme-red: #D11111;
+$font-size-responsive: 3vw;
+$font-size-fixed: 65px;
 
 @media (max-width: 1920px) {
   .content-brief-text {
-    font-size: 3vw;
+    font-size: $font-size-responsive;
+    div {
+      height: 3.5vw;
+    }
   }
 }
 
 @media (min-width: 1920px) {
   .content-brief-text {
-    font-size: 70px;
+    font-size: $font-size-fixed;
+    div {
+      height: 90px;
+    }
   }
 }
 
 .content-brief-page {
   position: relative;
+  display: flex;
   width: 100vw;
   height: 100vh;
   background-image: url('../assets/images/content-brief/content_brief_background.png');
@@ -42,14 +82,31 @@ $theme-red: #D11111;
 }
 
 .content-brief-text {
-  position: absolute;
-  top: 30vh;
-  left: 50%;
-  transform: translateX(-50%);
-  width: 100%;
+  position: relative;
+  margin: auto;
+  width: fit-content;
   height: fit-content;
-  max-width: 1100px;
   color: $theme-red;
   font-weight: 600;
+  overflow: hidden;
+
+  div {
+    position: relative;
+    // display: flex;
+    // justify-content: center;
+    overflow: hidden;
+    margin-bottom: 20px;
+
+    p {
+      margin-top: 100px;
+      transition: margin-top .8s ease-out;
+    }
+  }
+
+  &.show-brief-text {
+    p {
+      margin-top: 0px;
+    }
+  }
 }
 </style>
