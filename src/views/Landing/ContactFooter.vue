e0dd30b4223e80bc07ce6f9a25ec806a044bf47d
Jakkaphan Payuhakait <tee@dept.email>
2021-04-19 23:32:11 +0700

    add contact footer

---
+++ src/views/Landing/ContactFooter.vue
@@ -0,0 +1,166 @@
+<template>
+  <div class="contact-footer-bar">
+    <img class="contact-brand-logo" :src="require('@/assets/images/contact-footer/brand_logo_red.svg')" />
+    <div class="contact-form" align="left">
+      <div align="right">
+        รัตโกสินทร์ศก ๒๔๐
+      </div>
+      <div align="left">
+        <p>ถึงคุณ ชญานี จันทร์อินทร์</p>
+      </div>
+      <div>
+        <b-input class="contact-input" autocomplete="off" block />
+      </div>
+      <div class="mt-2" align="right">
+        <p>ด้วยความเคารพยิ่ง</p>
+      </div>
+      <div class="contact-form-foot" align="right">
+        <div>
+          ท่าน
+        </div>
+        <b-input class="contact-input small-input" autocomplete="off" size="sm" />
+      </div>
+    </div>
+    <div class="producer-profile-container">
+      <img :src="require('@/assets/images/contact-footer/producer.jpeg')" />
+      <div class="producer-name mt-1">
+        <strong>
+          นางสาว ชญานี จันทร์อินทร์
+        </strong>
+      </div>
+      <div class="producer-major">
+        สาขาการออกแบบสื่อปฏิสัมพันธ์และมัลติมีเดีย, มศว
+      </div>
+    </div>
+    <div class="producer-contact-data" align="left">
+      <h3>
+        ติดต่อ
+      </h3>
+      <div class="producer-phone">
+        โทร. 082-2190795
+      </div>
+      <div class="producer-email">
+        chayanee.chanin@gmail.com
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContactFooter',
+  data() {
+    return {
+      //
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@media (max-width: 1920px) {
+  .contact-form {
+    font-size: 1.5vw;
+  }
+  .producer-name {
+    font-size: .9vw;
+  }
+  .producer-major {
+    font-size: .8vw;
+  }
+  .producer-phone {
+    font-size: 1.3vw;
+  }
+  .producer-email {
+    font-size: 1vw;
+  }
+}
+
+@media (min-width: 1920px) {
+  .contact-form {
+    font-size: 28px;
+  }
+  .producer-name {
+    font-size: 18px;
+  }
+  .producer-major {
+    font-size: 15px;
+  }
+  .producer-phone {
+    font-size: 24px;
+  }
+  .producer-email {
+    font-size: 18px;
+  }
+}
+
+.contact-footer-bar {
+  position: relative;
+  width: 100vw;
+  height: 50vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: url('../../assets/images/contact-footer/footer_background.png');
+  background-size: contain;
+  background-repeat: repeat-x;
+  overflow: hidden;
+  color: white;
+}
+.contact-brand-logo {
+  position: absolute;
+  top: 15%;
+  width: 4vw;
+  max-width: 90px;
+  max-height: 90px;
+}
+.contact-form {
+  position: absolute;
+  top: 30%;
+  width: 40vw;
+  height: fit-content;
+  max-width: 750px;
+  .contact-form-foot {
+    position: relative;
+    div {
+      height: 100%;
+      display: inline-block;
+      margin-right: 10px;
+    }
+    input {
+      display: inline-block;
+    }
+  }
+}
+.contact-input {
+  background-color: #C4C4C4;
+  border-radius: 0;
+  &:focus {
+    box-shadow: none;
+    outline: none;
+  }
+  &.small-input {
+    max-width: 50%;
+  }
+}
+.producer-profile-container {
+  position: absolute;
+  top: 30%;
+  right: 7%;
+  width: 11vw;
+  height: 280px;
+  max-width: 300px;
+  img {
+    width: 100%;
+  }
+}
+.producer-contact-data {
+  position: absolute;
+  top: 38%;
+  left: 7%;
+  width: 18vw;
+  h3 {
+    font-weight: 600;
+  }
+}
+</style>
--- src/views/Landing/LandingPage.vue
+++ src/views/Landing/LandingPage.vue
@@ -1,26 +1,41 @@
 <template>
-  <div>
+  <div class="landing-page">
     <Home id="home-page" @scrollToPage="scrollToPage" />
     <ContentIntro id="content-intro-page" />
     <ContentFirst id="content-first-page" />
     <ContentSwitchRole id="content-switch-role-page" />
     <ContentBrief id="content-brief-page" />
     <QuizIntro id="quiz-intro-page" />
+    <ContactFooter id="contact-footer" />
   </div>
 </template>
 
 <script>
 // import scrollEventCheck from '@/helpers/scrollEventCheck.js'
 import { mapState, mapMutations } from 'vuex'
+import Home from './Home'
+import ContentIntro from './ContentIntro'
+import ContentFirst from './ContentFirst'
+import ContentSwitchRole from './ContentSwitchRole'
+import ContentBrief from './ContentBrief'
+import QuizIntro from '../Quiz/QuizIntro'
+import ContactFooter from './ContactFooter'
 
 export default {
   components: {
-    Home: () => import('./Home'),
-    ContentIntro: () => import('./ContentIntro'),
-    ContentFirst: () => import('./ContentFirst'),
-    ContentSwitchRole: () => import('./ContentSwitchRole'),
-    ContentBrief: () => import('./ContentBrief'),
-    QuizIntro: () => import('../Quiz/QuizIntro')
+    Home,
+    ContentIntro,
+    ContentFirst,
+    ContentSwitchRole,
+    ContentBrief,
+    QuizIntro,
+    ContactFooter
+    // Home: () => import('./Home'),
+    // ContentIntro: () => import('./ContentIntro'),
+    // ContentFirst: () => import('./ContentFirst'),
+    // ContentSwitchRole: () => import('./ContentSwitchRole'),
+    // ContentBrief: () => import('./ContentBrief'),
+    // QuizIntro: () => import('../Quiz/QuizIntro')
   },
   data() {
     return {
@@ -62,3 +77,9 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.landing-page {
+  scroll-snap-type: y mandatory;
+}
+</style>
